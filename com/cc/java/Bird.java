f5d2e5831cc013c7041193c92bc3186f5f6be190
badawi <Bdwahmed@gmail.com>
2024-05-29 12:18:49 +0200

    bird() subklasse

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+        
+        return " But i am a Bird";
+    }
+
+    @Override
+    public String fly() {
+       
+        return "i can fly at 20mph!";
+    }
+
+
+
+    
+
+}
